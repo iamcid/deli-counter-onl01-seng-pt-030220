da0cdfce2cefabb7ec761a0836dffce16bfa5085
Christopher Cid <iamcid17@gmail.com>
2020-03-11 04:58:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,6 @@ def line(katz_deli)
   end
 end 
 
-def take_a_number(katz_deli, )
-  line = []
-  if katz_deli.length == 0
-    puts "Welcome, #{name}. You are number #{index + 1} in line."
-  end
+def take_a_number(katz_deli, name)
+  katz_deli << name
 end
