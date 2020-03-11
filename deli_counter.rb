a96a94f5491dfa25299c89455fb36f9d820d36dc
Christopher Cid <iamcid17@gmail.com>
2020-03-11 04:21:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 
 def line(katz_deli)
-  customers = []
+  line = []
   if katz_deli.length == 0
     puts "The line is currently empty."
   else 
-    katz_deli.each_with_index do |name, index|
+    .each_with_index do |name, index|
       customers.push("#{index + 1}. #{name}")
   end
   puts "The line is currently:" << "#{customers.join(" ")}"
